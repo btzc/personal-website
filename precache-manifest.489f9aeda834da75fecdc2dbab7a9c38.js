31ffcdbf33f6f69824a5ef003cc947a4ad2983cd
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2020-05-05 22:57:39 -0700

    Updates

---
+++ precache-manifest.489f9aeda834da75fecdc2dbab7a9c38.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f90dc35435e63eb112942758db4206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b87392dc11a7c48d8e",
+    "url": "/static/css/main.767945e4.chunk.css"
+  },
+  {
+    "revision": "1c25167c4f304ba2155d",
+    "url": "/static/js/2.1461a18d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1461a18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b87392dc11a7c48d8e",
+    "url": "/static/js/main.e39acc8e.chunk.js"
+  },
+  {
+    "revision": "9d7ec453320509f6d66c",
+    "url": "/static/js/runtime-main.42cfb24a.js"
+  },
+  {
+    "revision": "d046da01d985d38f1ae79da096fce6eb",
+    "url": "/static/media/0E2A3720.d046da01.JPG"
+  },
+  {
+    "revision": "86d4c7434df9ab4999d865dfe874e866",
+    "url": "/static/media/crwn-clothing.86d4c743.PNG"
+  },
+  {
+    "revision": "b1de6401bd6b320635e08b86a5d812f3",
+    "url": "/static/media/javascript_example.b1de6401.PNG"
+  },
+  {
+    "revision": "92799c310aa7f4ff776a3a3ba2dc7ce8",
+    "url": "/static/media/personal-website.92799c31.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.4571197f7371e6505682970b7a86d38c.js"
+  "/precache-manifest.489f9aeda834da75fecdc2dbab7a9c38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
